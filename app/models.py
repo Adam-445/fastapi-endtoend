5cfa1391a441bcbadb303b0e2af19a3dcbcb5604
Adam-445 <Adamch012016@outlook.com>
2025-01-28 10:58:35 +0100

    Fixed even more bugs

--- app/models.py
+++ app/models.py
@@ -35,11 +35,11 @@ class User(Base):
     )
 
 
-class Vote(Base):
-    __tablename__ = "votes"
-    user_id = Column(
-        Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True
-    )
-    post_id = Column(
-        Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True
-    )
+# class Vote(Base):
+#     __tablename__ = "votes"
+#     user_id = Column(
+#         Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True
+#     )
+#     post_id = Column(
+#         Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True
+#     )
