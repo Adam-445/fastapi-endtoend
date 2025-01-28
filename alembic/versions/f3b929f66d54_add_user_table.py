5fa1ec1145dd0de5e927ec192ae2028c2d64aae1
Adam-445 <Adamch012016@outlook.com>
2025-01-28 10:50:06 +0100

    Fixed bugs

--- alembic/versions/f3b929f66d54_add_user_table.py
+++ alembic/versions/f3b929f66d54_add_user_table.py
@@ -23,8 +23,8 @@ def upgrade() -> None:
     op.create_table(
         'users',
         sa.Column("id", sa.Integer(), nullable=False),
-        sa.Column("email", sa.Integer(), nullable=False),
-        sa.Column("password", sa.Integer(), nullable=False),
+        sa.Column("email", sa.String(), nullable=False),
+        sa.Column("password", sa.String(), nullable=False),
         sa.Column(
             "created_at",
             sa.TIMESTAMP(timezone=True),
